cee1c85edf535e7e6d4bd7e753b259edc7354cd3
DANIEL\danie <dani25guerra@gmail.com>
2022-10-12 23:55:18 -0500

    agregado laboratorio8

---
+++ Lab8/index.js
@@ -0,0 +1,8 @@
+function calcular(){
+  const Cadena = document.getElementById("texto").value
+  document.getElementById("respuesta").style.visibility="visible";
+  document.getElementById("respuesta").innerHTML="letras";
+  
+  }
+  function limpiar(){
+  
